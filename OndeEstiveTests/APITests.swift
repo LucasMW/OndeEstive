1d5f4b2c7c58606ee7bfe5c8282a85204adb376c
LucasMW <great-lucas@hotmail.com>
2021-01-10 19:25:42 +0000

    tests

--- OndeEstiveTests/APITests.swift
+++ OndeEstiveTests/APITests.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2020 Lucas Menezes. All rights reserved.
 //
 
-import XCTest
 
+import XCTest
+@testable import OndeEstive
 class APITests: XCTestCase {
 
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,9 +20,47 @@ class APITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInsertCoordinate() {
+       let api = API()
+        let exp = XCTestExpectation()
+        api.insertCoordinates(personId: 0, date: Date.init(timeIntervalSinceNow: 0), lat: 0.0, long: 0.17) { (isWorking) in
+            XCTAssert(isWorking)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
+        
+    }
+    func testInsertBulkCoordinate() {
+       let api = API()
+        let exp = XCTestExpectation()
+        let dummyArray = [1.0,2.1,3.1,4.1,5.1]
+        let array = dummyArray.map { (u) -> CoordinateParam in
+            return CoordinateParam(personId: 0, date: Date.init(timeIntervalSinceNow: 0), lat: Float(u/100.0), long: Float(u/100.0))
+        }
+        print(array)
+        
+        api.insertBulkCoordinates(array: array) { (isWorking) in
+            XCTAssert(isWorking)
+                exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
+        
+    }
+    func testInsertBulkCoordinate2() {
+       let api = API()
+        let exp = XCTestExpectation()
+        let dummyArray = [1.0,2.1,3.1,4.1,5.1]
+        let array = dummyArray.map { (u) -> CoordinateParam in
+            return CoordinateParam(personId: 0, date: Date.init(timeIntervalSinceNow: 0), lat: Float(u/100.0), long: Float(u/100.0))
+        }
+        print(array)
+        
+        api.insertBulkCoordinates2(array: array) { (isWorking) in
+            XCTAssert(isWorking)
+                exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
+        
     }
 
     func testPerformanceExample() {
